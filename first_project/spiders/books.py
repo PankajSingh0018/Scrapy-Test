67f5e77686add332ffe39a85a3e9c20d4be7aad2
PankajSingh0018 <singh.pankaj0018@gmail.com>
2023-07-15 22:09:10 +0530

    comments added

--- first_project/spiders/books.py
+++ first_project/spiders/books.py
@@ -2,24 +2,42 @@ import scrapy
 from pathlib  import Path
 import pandas as pd 
 
-
+# BooksSpider Class is defined that is inherited from the scrapy.Spider
 class BooksSpider(scrapy.Spider):
+    # Setting the Spider name to books
     name = "books"
+    
+    # Setting the allowed domain and start url for the spider 
+    # These urls are the entry points for the spider to start crawl 
     allowed_domains = ["toscrape.com"]
     start_urls = ["https://toscrape.com"]
 
+    """ defining start_requests function to generate the initial requests to start the crawler.
+    """
     def start_requests(self):
         urls = [
             "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html",
             "http://books.toscrape.com/catalogue/category/books/travel_2/index.html",
         ]
         for url in urls:
+            # It Yields a scrapy.Request object for each url in the urls list.
+            # The callback parameter specifies the function to be called when a reponse is received from each requests
             yield scrapy.Request(url=url, callback= self.parse)
-
+    
+    """ Defining the parse function which gets invoked when a response is received for a request
+    It is basically responsible for extracting the data and saving it 
+    """
     def parse(self, response):
+        # Extract the page name from the response URL
         page = response.url.split("/")[-2]
+        
+        # Generating the file name 
         filename = f"Books-{page}.html"
+        
+        #Log a message to indicate that the file is saved.
         self.log(f"Saved file {filename}")
+
+        #Creating an empty list to append all the details
         details = []
         cards = response.css(".product_pod")
 
